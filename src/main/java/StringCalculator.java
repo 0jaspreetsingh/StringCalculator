73c0886cfd48b08254f539efee8a5905ce012068
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2021-02-08 23:43:25 +0530

    defined basic add function

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,10 +5,29 @@ public class StringCalculator {
 
     /**
      * utility function to add numbers given in string input seperated by comma or some other specified character
+     *
      * @param input
      * @return
      */
     public int add(String input) {
-        return 0;
+        return this.getSum(input, ",");
+    }
+
+
+    /**
+     * splits input to an array depending on the seperator and returns sum
+     * @param input
+     * @param seperator
+     * @return
+     */
+    private int getSum(String input, String seperator) {
+        if (input == "" || input.trim() == "") return 0;
+        String inputArray[] = input.split(seperator);
+        int answer = 0;
+
+        for (String inputStringValue : inputArray) {
+            answer += Integer.parseInt(inputStringValue);
+        }
+        return answer;
     }
 }
