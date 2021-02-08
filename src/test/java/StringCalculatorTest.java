d3961adb555f5c91e3f4a88de6bb1b03c0b1eba4
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2021-02-09 01:15:25 +0530

    added problem 5 test case

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Test class for StringCalculator
@@ -12,7 +13,7 @@ public class StringCalculatorTest {
      * problem 1
      */
     @Test
-    public void add_ShouldReturnZeroForEmptyString() {
+    public void add_ShouldReturnZeroForEmptyString() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "";
         int expected = 0;
@@ -24,7 +25,7 @@ public class StringCalculatorTest {
      * problem 1
      */
     @Test
-    public void add_ShouldReturnNumberItselfForInputWithSingleValue() {
+    public void add_ShouldReturnNumberItselfForInputWithSingleValue() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "2";
         int expected = 2;
@@ -36,7 +37,7 @@ public class StringCalculatorTest {
      * problem 2
      */
     @Test
-    public void add_ShouldReturnSumForInputWithTwoOrMoreCommaSeperatedNumbers() {
+    public void add_ShouldReturnSumForInputWithTwoOrMoreCommaSeperatedNumbers() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "2,3,6,7";
         int expected = 18;
@@ -48,7 +49,7 @@ public class StringCalculatorTest {
      * problem 3
      */
     @Test
-    public void add_ShouldHandleInputCorrectlyWithNewLineCharacters() {
+    public void add_ShouldHandleInputCorrectlyWithNewLineCharacters() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "1\n2,3";
         int expected = 6;
@@ -60,7 +61,7 @@ public class StringCalculatorTest {
      * problem 3
      */
     @Test
-    public void add_ShouldHandleInputCorrectlyWithMultipleNewLineCharacters() {
+    public void add_ShouldHandleInputCorrectlyWithMultipleNewLineCharacters() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "1\n2,3,5\n7";
         int expected = 18;
@@ -72,7 +73,7 @@ public class StringCalculatorTest {
      * problem 4
      */
     @Test
-    public void add_ShouldHandleinputCorrectlyForDifferentSeperatorSpecifiedAtBeginning() {
+    public void add_ShouldHandleinputCorrectlyForDifferentSeperatorSpecifiedAtBeginning() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "//;\n1;2";
         int expected = 3;
@@ -84,11 +85,25 @@ public class StringCalculatorTest {
      * problem 4
      */
     @Test
-    public void add_ShouldHandleinputCorrectlyForDifferentSeperatorSpecified() {
+    public void add_ShouldHandleinputCorrectlyForDifferentSeperatorSpecified() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         String addInput = "//^\n1^2";
         int expected = 3;
         int actual = stringCalculator.add(addInput);
         assertEquals(expected, actual, "Should handle input correctly for different seperator specified");
+    }
+
+    /**
+     * problem 4
+     */
+    @Test
+    public void add_ShouldThrowExceptionForNegativeNumbersInInput() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        String addInput = "//^\n1^-2";
+        int expected = 3;
+
+        assertThrows(Exception.class, () -> {
+            stringCalculator.add(addInput);
+        });
     }
 }
